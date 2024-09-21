aaa593127be23dc08feb4a789b5c1039ece80a06
GDWR <gregory.dwr@gmail.com>
2024-09-21 13:51:31 +0100

    Use local mux rather than default global mux

--- internal/http/server.go
+++ internal/http/server.go
@@ -20,25 +20,26 @@ type Server struct {
 }
 
 func (s *Server) Run() error {
+	mux := http.NewServeMux()
 	middleware := middleware.LoggerMiddleware()
 
 	environmentsHandler := middleware(environments.NewHandler(s.DatabaseService, s.DockerService))
-	http.Handle("GET /api/v1/environments", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/configs", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/containers", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/images", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/networks", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/system-information", environmentsHandler)
-	http.Handle("GET /api/v1/environments/{id}/volumes", environmentsHandler)
+	mux.Handle("GET /api/v1/environments", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/configs", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/containers", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/images", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/networks", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/system-information", environmentsHandler)
+	mux.Handle("GET /api/v1/environments/{id}/volumes", environmentsHandler)
 
 	systemHandler := middleware(system.NewHandler(s.DatabaseService, s.UserService))
-	http.Handle("GET /api/v1/system", systemHandler)
-	http.Handle("POST /api/v1/system/initial-setup", systemHandler)
+	mux.Handle("GET /api/v1/system", systemHandler)
+	mux.Handle("POST /api/v1/system/initial-setup", systemHandler)
 
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	mux.Handle("/", http.FileServer(http.Dir("static")))
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		return err
 	}
 
