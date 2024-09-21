33f87492cbd6ffb4a3b8a72f0ec8d59b50bd077d
GDWR <gregory.dwr@gmail.com>
2024-09-21 10:59:19 +0100

    Log errors in internal server error responses

--- internal/http/response/internal_server_error.go
+++ internal/http/response/internal_server_error.go
@@ -2,8 +2,14 @@ package response
 
 import (
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 func InternalServerError(err error, message string, w http.ResponseWriter) {
+	log.Error().
+		Err(err).
+		Msg(message)
+
 	Error(message, http.StatusInternalServerError, w)
 }
